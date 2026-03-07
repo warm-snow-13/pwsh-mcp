9a164a0f0f8cc8f869259a908d77812521428615
igor.stepanushko <igor.stepanushko@outlook.com>
2026-03-07 21:36:53 +0200

    refactor: add tests for utils.set_version function

--- scripts/PSCoreUtils/PSCoreUtils.tests.ps1
+++ scripts/PSCoreUtils/PSCoreUtils.tests.ps1
@@ -21,3 +21,42 @@ Describe 'Environment Check' {
     }
 }
 
+Describe 'utils.set_version' {
+    It 'Should increment build by 1 by default' {
+        $result = utils.set_version -Version ([version]'1.2.3')
+
+        $result | Should -Be ([version]'1.2.4')
+    }
+
+    It 'Should increment major and reset minor/build' {
+        $result = utils.set_version -Version ([version]'1.2.3') -Part 'Major'
+
+        $result | Should -Be ([version]'2.0.0')
+    }
+
+    It 'Should increment minor and reset build' {
+        $result = utils.set_version -Version ([version]'1.2.3') -Part 'Minor'
+
+        $result | Should -Be ([version]'1.3.0')
+    }
+
+    It 'Should increment build when part is Build' {
+        $result = utils.set_version -Version ([version]'1.2.3') -Part 'Build'
+
+        $result | Should -Be ([version]'1.2.4')
+    }
+
+    It 'Should increment by custom value' {
+        $result = utils.set_version -Version ([version]'1.2.3') -Part 'Build' -IncrementBy 5
+
+        $result | Should -Be ([version]'1.2.8')
+    }
+
+    It 'Should throw when part is invalid' {
+        { utils.set_version -Version ([version]'1.2.3') -Part 'Patch' } | Should -Throw
+    }
+
+    It 'Should throw when increment is less than 1' {
+        { utils.set_version -Version ([version]'1.2.3') -IncrementBy 0 } | Should -Throw
+    }
+}
